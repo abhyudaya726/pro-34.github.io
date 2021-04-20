62449cb3524670deef8fcd3950854dfc38e5c4e8
abhyudaya726 <71880557+abhyudaya726@users.noreply.github.com>
2021-04-20 13:05:14 +0530

    Update Chain.js

--- Chain.js
+++ Chain.js
@@ -1,21 +1,27 @@
-class Chain{
-    constructor(bodyA, pointB){
-        var options = {
-            bodyA: bodyA,
-            pointB: pointB,
-            stiffness: 0.04,
-            length: 200
-        }
-        this.pointB = pointB;
-        this.sling = Constraint.create(options);
-        World.add(world, this.sling);
+class Pendulum{
+    constructor(x,y, color){
+        var options ={
+           ' restitution': 1,
+           'friction': 0,
+           'frictionAir':0.0,
+           'slop':1,
+           'inertia': Infinity
+        };
+        this.body = Bodies.rectangle(x, y, 40,40, options);
+        this.x= x;
+        this.y= y;
+        this.color = color;
+        World.add(world, this.body);
     }
-
-
-    display(){
-        var pointA = this.sling.bodyA.position;
-        var pointB = this.pointB;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    display() {
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+        fill(this.color);
+        ellipse(0, 0, 60, 60);
+        pop();
     }
 }
